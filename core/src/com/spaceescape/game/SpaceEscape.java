07d22f915a3bf4600520e95d9383b402316b8833
Robson <robsonmsoares88@gmail.com>
2019-07-01 22:03:20 -0300

    1.11 icone alterado

--- core/src/com/spaceescape/game/SpaceEscape.java
+++ core/src/com/spaceescape/game/SpaceEscape.java
@@ -20,7 +20,7 @@ import java.util.Random;
 public class SpaceEscape extends ApplicationAdapter {
 
     // Alterar aqui antes de comitar
-    //# Versão 1.10
+    //# Versão 1.11
 
     // TEXTURAS
     private SpriteBatch batch;
